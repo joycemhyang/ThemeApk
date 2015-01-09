58fc9d0720f417765a0847ca821820702b1c7a11
Joyce.Yang <Joyce.Yang@Qisda.com>
2015-01-09 14:36:39 +0800

    Add modules

---
+++ QissDay/src/main/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_PACKAGE_NAME := normalQissDay
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/overlay/theme/theme_normal
+
+LOCAL_AAPT_FLAGS := -o
+
+include $(BUILD_PACKAGE)
